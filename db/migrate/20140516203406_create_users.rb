423c856caac1adf255b273ce29ae490af242efba
Katsunori SUZUI <kt@7kry.net>
2014-05-17 05:34:17 +0900

    Fix model `user'

--- db/migrate/20140516203406_create_users.rb
+++ db/migrate/20140516203406_create_users.rb
@@ -1,14 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.integer :user_id
+      t.uid :provider
+      t.string :user_id
       t.string :screen_name
       t.string :name
-      t.string :profile_image
+      t.binary :profile_image
       t.string :descripton
-      t.boolean :link_twitter
 
       t.timestamps
     end
+    add_index :users, :provider
+    add_index :users, :user_id
   end
 end
