172394577373e63d749495928f0023286552ded1
Katsunori SUZUI <kt@7kry.net>
2014-07-21 06:20:08 +0900

    bugfix
    ユーザページを開いた時に、toolbarのユーザ名が開いているユーザの情報になるやつ (非常に問題ある)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,6 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    @user ||= User.find(session[:user_id]) if session[:user_id]
+    User.find(session[:user_id]) if session[:user_id]
   end
 end
